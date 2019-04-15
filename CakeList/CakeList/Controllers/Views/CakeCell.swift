d9834491872f7bd0143245f97704dbc1687ce47b
Adam Bandeira Lynas <adam@limitbreak.tech>
2019-04-15 19:59:55 +0100

    Added CakeCell

---
+++ CakeList/CakeList/Controllers/Views/CakeCell.swift
@@ -0,0 +1,17 @@
+//
+//  CakeCell.swift
+//  CakeList
+//
+//  Created by Adam Bandeira Lynas on 15/04/2019.
+//  Copyright © 2019 Limit Break Technology Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CakeItem {
+    var cakeTitle : String
+    var cakeDescription : String
+    var cakeImage : UIImage
+}
+
